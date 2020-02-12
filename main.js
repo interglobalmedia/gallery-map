dc4c8f4585142e6dfbb72cb2b036ff15c1d596c7
interglobalmedia <interglobalmedia@gmail.com>
2020-02-12 07:53:55 -0500

    main.css and main.js placed in new directory

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 let images = [
-  "images/david-clode-RAfIk-ZKbPk-unsplash.jpg",
-  "images/lesly-juarez-4JDPLjR1Ew4-unsplash.jpg",
-  "images/neil-cooper-uwI8R_FyLrI-unsplash.jpg",
-  "images/sophia-muller-loP8xxkZIk8-unsplash.jpg",
-  "images/itengyart-Pm2wSt4WwgI-unsplash.jpg"
+  "assets/images/david-clode-RAfIk-ZKbPk-unsplash.jpg",
+  "assets/images/lesly-juarez-4JDPLjR1Ew4-unsplash.jpg",
+  "assets/images/neil-cooper-uwI8R_FyLrI-unsplash.jpg",
+  "assets/images/sophia-muller-loP8xxkZIk8-unsplash.jpg",
+  "assets/images/itengyart-Pm2wSt4WwgI-unsplash.jpg"
 ];
 
 // creates the div and img tag and then imports the images
